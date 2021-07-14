0e67422831211071c4c47c0d24299a66fe354e81
gizeasy <gizeasy@gmail.com>
2021-07-14 17:59:35 +0300

    chore(gren): add config

---
+++ .grenrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  dataSource: 'commits',
+  prefix: '',
+  includeMessages: 'commits',
+  ignoreCommitsWith: ['merge', 'Merge', 'release', 'changelog', 'not-changelog'],
+  ignoreTagsWith: [],
+  changelogFilename: 'CHANGELOG.md',
+  template: {
+    commit: ({ message, url, author, name }) =>
+      `- [${message}](${url}) - ${author ? `[@${author}](https://github.com/${author})` : name}`,
+    issue: '- {{labels}} {{name}} [{{text}}]({{url}})',
+    label: '[**{{label}}**]',
+    noLabel: 'closed',
+    group: '\n#### {{heading}}\n',
+    changelogTitle: '# Changelog\n\n',
+    release: '## {{release}} ({{date}})\n{{body}}',
+    releaseSeparator: '\n---\n\n',
+  },
+}
