f56a34a3584c4012bc1dee36cfbbb19cf05e035d
Anya Ovsyannikova <arhayka@gmail.com>
2021-08-04 11:05:25 +0300

    docs(Bullet): add docs and examples
    issue -  1388

--- src/Bullet/__stories__/examples/BulletExample/BulletExample.tsx
+++ src/Bullet/__stories__/examples/BulletExample/BulletExample.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { data } from '../../../__mocks__/mock.data'
+import { data, dataSingle } from '../../../__mocks__/mock.data'
 import { Bullet } from '../../../Bullet'
 
 export function BulletExample() {
@@ -11,6 +11,37 @@ export function BulletExample() {
       rangeField="ranges"
       targetField="target"
       xField="title"
+    />
+  )
+}
+
+export function BulletExampleSingle() {
+  return (
+    <Bullet
+      style={{
+        height: '140px',
+      }}
+      data={dataSingle}
+      measureField="mesures"
+      rangeField="range"
+      targetField="target"
+      xField="title"
+    />
+  )
+}
+
+export function BulletExampleNoAxys() {
+  return (
+    <Bullet
+      style={{
+        height: '100px',
+      }}
+      data={dataSingle}
+      measureField="mesures"
+      rangeField="range"
+      targetField="target"
+      xField="title"
+      yAxis={false}
     />
   )
 }
