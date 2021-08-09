56a57bdc3b19b593976a06aca80767ac230d2057
Anya Ovsyannikova <arhayka@gmail.com>
2021-08-09 12:56:56 +0300

    docs(RemadeInfo): edit text, add the second informer

--- src/__private__/storybook/components/RemadeInfo/RemadeInfo.tsx
+++ src/__private__/storybook/components/RemadeInfo/RemadeInfo.tsx
@@ -14,11 +14,11 @@ export const RemadeInfo: React.FC<{ apiUrl: string; commponentName: string }> = 
   return (
     <Informer className={cnRemadeInfo()} status="system" view="bordered">
       <Text>
-        Компонент{' '}
+        Это компонент{' '}
         <Text as="span" weight="bold">
           {commponentName}
         </Text>{' '}
-        взят из{' '}
+        из библиотеки{' '}
         <Text
           className={cnRemadeInfo('Link')}
           as="a"
@@ -28,7 +28,7 @@ export const RemadeInfo: React.FC<{ apiUrl: string; commponentName: string }> = 
         >
           G2Plot
         </Text>
-        , добавлена тематизация из{' '}
+        , к нему добавили тематизацию из{' '}
         <Text
           className={cnRemadeInfo('Link')}
           as="a"
@@ -36,11 +36,29 @@ export const RemadeInfo: React.FC<{ apiUrl: string; commponentName: string }> = 
           href="https://consta.gazprom-neft.ru"
           target="_blank"
         >
-          Consta
-        </Text>
-        , и полностью проксировано{' '}
+          дизайн-системы Consta
+        </Text>{' '}
+        и полностью проксировали API.
+      </Text>
+      <Text style={{ marginTop: 'var(--space-m)' }}>
+        Здесь описаны только основные свойства, подробности — в{' '}
         <Text className={cnRemadeInfo('Link')} as="a" view="link" href={apiUrl} target="_blank">
-          Api
+          документации G2Plot
+        </Text>
+        .
+      </Text>
+    </Informer>
+  )
+}
+
+export const RemadeTableInfo: React.FC<{ apiUrl: string }> = props => {
+  const { apiUrl } = props
+  return (
+    <Informer className={cnRemadeInfo()} status="system" view="bordered">
+      <Text style={{ marginBottom: 'var(--space-m)' }}>
+        Здесь описаны только основные свойства компонента.{' '}
+        <Text className={cnRemadeInfo('Link')} as="a" view="link" href={apiUrl} target="_blank">
+          Посмотреть все свойства
         </Text>
       </Text>
     </Informer>
