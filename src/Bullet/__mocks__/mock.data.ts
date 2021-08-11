92664d5414fba1fdc1cf2cae7fdeaf03ff048ff9
arhayka <harlond@mail.ru>
2021-08-11 19:20:23 +0300

    docs(Bullet): add docs and examples (#8)
    issue -  https://github.com/gazprom-neft/consta-uikit/issues/1388

--- src/Bullet/__mocks__/mock.data.ts
+++ src/Bullet/__mocks__/mock.data.ts
@@ -5,3 +5,8 @@ export const data = [
   { title: 'bullet4', ranges: [100], measures: [30], target: 60 },
   { title: 'bullet5', ranges: [100], measures: [45], target: 85 },
 ]
+
+export const dataSingle = [
+  { title: 'Наша вторая цель', range: [100], mesures: [30], target: 70 },
+  { title: 'Наша первая цель', range: [50, 100], mesures: [5, 20, 30, 25], target: 85 },
+]
