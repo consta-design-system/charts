08fe8eea311ee04146540ebf150380121c060d72
Anya Ovsyannikova <arhayka@gmail.com>
2021-08-20 16:14:02 +0300

    docs(Waterfall): edit docs, add props table and examples
    issue -  #1368

--- src/Waterfall/__stories__/examples/WaterfallExample/WaterfallExample.tsx
+++ src/Waterfall/__stories__/examples/WaterfallExample/WaterfallExample.tsx
@@ -9,6 +9,7 @@ export function WaterfallExample() {
   const vars = useThemeVars()
   return (
     <Waterfall
+      style={{ marginBottom: 'var(--space-xl)' }}
       data={data}
       xField="type"
       yField="money"
@@ -33,6 +34,97 @@ export function WaterfallExample() {
               lineDash: null,
               stroke: vars.color.primary['--color-bg-system'],
             },
+          },
+        },
+      }}
+    />
+  )
+}
+
+export function WaterfallExampleColor() {
+  const vars = useThemeVars()
+  return (
+    <Waterfall
+      style={{ marginBottom: 'var(--space-xl)' }}
+      data={data}
+      xField="type"
+      yField="money"
+      risingFill={vars.color.primary['--color-bg-success']}
+      fallingFill={vars.color.primary['--color-bg-alert']}
+      total={{
+        style: {
+          fill: vars.color.primary['--color-bg-system'],
+        },
+      }}
+    />
+  )
+}
+
+export function WaterfallExampleSimple() {
+  return (
+    <Waterfall
+      style={{ marginBottom: 'var(--space-xl)' }}
+      data={data}
+      xField="type"
+      yField="money"
+    />
+  )
+}
+
+export function WaterfallExampleLegend() {
+  const vars = useThemeVars()
+  return (
+    <Waterfall
+      style={{ marginBottom: 'var(--space-xl)' }}
+      data={data}
+      xField="type"
+      yField="money"
+      legend={{
+        layout: 'horizontal',
+        position: 'top-left',
+        marker: {
+          symbol: 'square',
+        },
+      }}
+      risingFill={vars.color.primary['--color-bg-success']}
+      fallingFill={vars.color.primary['--color-bg-alert']}
+      total={{
+        style: {
+          fill: vars.color.primary['--color-bg-system'],
+        },
+      }}
+    />
+  )
+}
+
+export function WaterfallExampleMeta() {
+  const vars = useThemeVars()
+  return (
+    <Waterfall
+      style={{ marginBottom: 'var(--space-xl)' }}
+      data={data}
+      xField="type"
+      yField="money"
+      legend={{
+        layout: 'horizontal',
+        position: 'top-left',
+        marker: {
+          symbol: 'square',
+        },
+      }}
+      risingFill={vars.color.primary['--color-bg-success']}
+      fallingFill={vars.color.primary['--color-bg-alert']}
+      total={{
+        style: {
+          fill: vars.color.primary['--color-bg-system'],
+        },
+        label: 'Всего',
+      }}
+      meta={{
+        money: {
+          alias: 'На счете',
+          formatter: v => {
+            return `${v} ₽`
           },
         },
       }}
