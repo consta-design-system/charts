ad446f26e6db5590aab05e188f75f353017b06ad
arhayka <arhayka@gmail.com>
2021-08-25 21:38:32 +0300

    docs(Waterfall): edit docs, add props table and examples (#16)
    issue -  #1368

--- src/Waterfall/__mocks__/data.mock.ts
+++ src/Waterfall/__mocks__/data.mock.ts
@@ -1,9 +1,9 @@
 export const data = [
-  { type: 'type-1', money: 120 },
-  { type: 'type-2', money: 900 },
-  { type: 'type-3', money: 200 },
-  { type: 'type-4', money: 300 },
-  { type: 'type-5', money: 1200 },
-  { type: 'type-6', money: 1000 },
-  { type: 'type-7', money: -2000 },
+  { type: 'Тип 1', money: 120 },
+  { type: 'Тип 2', money: 900 },
+  { type: 'Тип 3', money: 200 },
+  { type: 'Тип 4', money: 300 },
+  { type: 'Тип 5', money: 1200 },
+  { type: 'Тип 6', money: 1000 },
+  { type: 'Тип 7', money: -2000 },
 ]
