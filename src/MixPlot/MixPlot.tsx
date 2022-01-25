e7c5bc29761b3c525b8b2a401d51a1e7795259e6
N1MBER <socialfox33@gmail.com>
2022-01-25 20:09:50 +0300

    feat(MixPlot): add new chart
    issue -  #1888

---
+++ src/MixPlot/MixPlot.tsx
@@ -0,0 +1,36 @@
+import React, { forwardRef, useEffect, useImperativeHandle } from 'react'
+
+import { Mix } from '@antv/g2plot'
+import { useForkRef } from '@consta/uikit/useForkRef'
+
+import { useChart } from '@/__private__/hooks/useChart'
+import { useChartTheme } from '@/__private__/hooks/useChartTheme'
+import { getChart } from '@/__private__/utils/getChart'
+
+import { MixPlotComponentType } from './types'
+
+export * from './types'
+
+export const MixPlot: MixPlotComponentType = forwardRef((props, ref) => {
+  const {
+    chartRef,
+    style = {
+      height: 'inherit',
+    },
+    className,
+    ...rest
+  } = props
+  const theme = useChartTheme()
+  const { chart, container } = useChart(Mix, { ...rest, theme })
+
+  useEffect(() => {
+    getChart(chartRef, chart.current)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [chart.current])
+
+  useImperativeHandle(chartRef, () => ({
+    getChart: () => chart.current,
+  }))
+
+  return <div style={style} ref={useForkRef([ref, container])} className={className} />
+})
